5315a01e410647810d1a8a34a73862364127f575
lougerard <louis06.gerard@gmail.com>
2022-08-13 09:43:48 +0200

    adding cybersec

--- js/main.js
+++ js/main.js
@@ -17,7 +17,7 @@ exclude: 'yes'
         $('.panel-cover').animate({ 'max-width': '530px', 'width': '40%' }, 400, swing = 'swing', function () { })
       }
     })
-    $('a.events-button').click(function (e) {
+    /*$('a.events-button').click(function (e) {
       if ($('.panel-cover').hasClass('panel-cover--collapsed')) return
       currentWidth = $('.panel-cover').width()
       if (currentWidth < 960) {
@@ -27,7 +27,7 @@ exclude: 'yes'
         $('.panel-cover').css('max-width', currentWidth)
         $('.panel-cover').animate({ 'max-width': '530px', 'width': '40%' }, 400, swing = 'swing', function () { })
       }
-    })
+    })*/
 
     if (window.location.hash && window.location.hash == '#blog') {
       $('.panel-cover').addClass('panel-cover--collapsed')
